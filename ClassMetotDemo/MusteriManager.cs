d3fd13dc06e94b4e8db1dd37f700df78dce845e5
16 Ocak Ders Sonu <mustafaoztosun.g@gmail.com>
2021-02-10 08:18:39 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. Listeye Eklendi: " + musteri.Id + " : " + musteri.Adi + " : " + musteri.Soyadi + " : " + musteri.KimlikNo );
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 123546;
+            musteri1.Adi = "Mustafa";
+            musteri1.Soyadi = "Öztosun";
+            musteri1.KimlikNo = 11111111111;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1254646;
+            musteri2.Adi = "İsmail";
+            musteri2.Soyadi = "Akçuru";
+            musteri2.KimlikNo = 22222222222;
+
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3154665;
+            musteri3.Adi = "Tugay";
+            musteri3.Soyadi = "Şaşkan";
+            musteri3.KimlikNo = 33333333333;
+
+            Musteri[] musteriler = new Musteri[] {musteri1, musteri2, musteri3 };
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.KimlikNo);
+                Console.WriteLine("--------------------------------------");
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+
+        }
+    }
+}
